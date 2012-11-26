1e73f49604010a8adcd28510be72a48cb412c917
Chad Burggraf <chad.burggraf@gmail.com>
2012-11-26 11:07:20 -0700

    Initial commit.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿//--------------------------------------------------------------------------
+// 
+//  Copyright (c) Microsoft Corporation.  All rights reserved. 
+// 
+//  File: AssemblyInfo.cs
+//
+//--------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Parallel Extensions Extras")]
+[assembly: AssemblyDescription("Samples and extra functionality for use with Parallel Extensions to the .NET Framework")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Parallel Extensions Extras")]
+[assembly: AssemblyCopyright("Copyright © Microsoft Corporation.  All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ae914514-52ad-4769-a503-654f1a708dcc")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
